ad6adcde4bcc6bfaa2aa8dcc9e6b9a59bf520ef1
Vinu410 <vinayakjatti044@gmail.com>
2025-09-09 07:51:59 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,17 @@
+// Protect routes, save the page the user wanted
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
-    req.flash("error", "you must be logged in to create listings!!");
+    req.session.redirectUrl = req.originalUrl;
+    req.flash("error", "You must be logged in first!");
     return res.redirect("/login");
+  }
+  next();
+};
+
+// Make saved URL available for the login route
+module.exports.savedirectUrl = (req, res, next) => {
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl;
   }
   next();
 };
